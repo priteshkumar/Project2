2647036b1bdbc3ee27c192c285cfe4d3f315c173
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2017-11-21 15:58:44 -0800

    Modified html routes and added a second page

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -9,17 +9,13 @@ var path = require("path");
 module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   // add route loads the add.html page,
   // where users can enter new characters to the db
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add.html"));
+  app.get("/main", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/main.html"));
   });
-  // all route loads the all.html page,
-  // where all characters in the db are displayed
-  app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/all.html"));
-  });
+  
 };
